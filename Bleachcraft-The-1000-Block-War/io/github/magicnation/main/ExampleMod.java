ec0104980d826e1bed9ce1340c7111b751f11c99
juanwan11 <magicnation.juanwan11@gmail.com>
2015-09-26 19:47:23 -0700

    Baisc Classes

---
+++ Bleachcraft-The-1000-Block-War/io/github/magicnation/main/ExampleMod.java
@@ -0,0 +1,27 @@
+package io.github.magicnation.main;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Information.ModId, version = Information.Version)
+public class ExampleMod
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+    	
+    }
+}
---
+++ Bleachcraft-The-1000-Block-War/io/github/magicnation/main/Information.java
@@ -0,0 +1,6 @@
+package io.github.magicnation.main;
+
+public class Information {
+	public static final String ModId = "magicnation-bleachcraft";
+	public static final String Version = "1.0";
+}
